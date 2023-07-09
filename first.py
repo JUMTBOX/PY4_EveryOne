55b2a7a2aac9ce1681ab7c6def745c6383ea7c43
JUMTBOX <yjey12@naver.com>
2023-07-09 18:57:30 +0900

    ...

--- first.py
+++ first.py
@@ -1,1 +1,18 @@
 print("hello from a file")
+
+
+x = 2
+x = x + 2
+print(x)
+
+
+if x < 5:
+    print("smaller than five")
+if x > 1:
+    print("bigger than one")
+
+
+while x < 10:
+    print(x)
+    x = x + 1
+print("done!")
