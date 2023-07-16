89460a0dcdc4b1cf876031ac38a76ba1593d18aa
JUMTBOX <yjey12@naver.com>
2023-07-16 15:47:01 +0900

    ...

--- lecture2.py
+++ lecture2.py
@@ -1,7 +1,9 @@
-# inp = input("enter your name")
-# print("hello", inp)
+#practice1
 
+inp = input("enter your name")
+print("hello", inp)
 
+#practice2
 inp2 = input("시급")
 inp3 = input("하루 근로시간")
 print("당신의 일당은",int(inp2) * int(inp3),"원 입니다")
