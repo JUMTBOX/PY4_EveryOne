7f1e93a4dea583e1de4ccd194a859d1cbed8b0f2
JUMTBOX <yjey12@naver.com>
2023-08-14 14:23:22 +0900

    ...

--- first.py
+++ first.py
@@ -1,20 +1,20 @@
-# print("hello from a file")
+print("hello from a file")
 
-# x = 2
-# x = x + 2
-# print(x)
-
-
-# if x < 5:
-#     print("smaller than five")
-# if x > 1:
-#     print("bigger than one")
+x = 2
+x = x + 2
+print(x)
 
 
-# while x < 10:
-#     print(x)
-#     x = x + 1
-# print("done!")
+if x < 5:
+    print("smaller than five")
+if x > 1:
+    print("bigger than one")
+
+
+while x < 10:
+    print(x)
+    x = x + 1
+print("done!")
 
 num1 = 200
 print(num1)
