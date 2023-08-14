186e2ae35f17be498f749b6fc8158595b2e18086
JUMTBOX <yjey12@naver.com>
2023-08-14 15:08:18 +0900

    ...

--- prac.py
+++ prac.py
@@ -1,13 +1,35 @@
 # 변수 명명 규칙
-x = 3
-y = 7
-font1 = "돋움1"
-my_age = 28
-myAge = 28
+# x = 3
+# y = 7
+# font1 = "돋움1"
+# my_age = 28
+# myAge = 28
 
 # 정수타입 = int
 # 실수타입 = float
 
 # 소수점n자리까지 출력하는 방법(자동 반올림 됨)
-a = 2 / 3
-print("%.2f" % a)
+# a = 2 / 3
+# print("%.2f" % a)
+
+# b = "안녕"
+# print(type(a))
+# print(type(b))
+
+# num = 0
+# while num < 5:
+#     print(num)
+#     num += 1
+# print("while문 끝남")
+
+# a = 3
+# b = 20
+# c = b // a
+# print(c)
+
+height = 170
+weight = 64
+
+bmi = weight / (height**2)
+
+print(bmi)
